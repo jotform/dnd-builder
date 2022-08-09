4a76028fdb384c7219f701ad677e437c90a4ffbf
Aybüke Ceylan <aybukeceylan@jotform.com>
2022-08-09 15:47:04 +0300

    fix(text-editor): url input visibility issue fixed
    Co-Authored-By: ozsirma <37998376+ozsirmajotform@users.noreply.github.com>

--- src/components/TextEditor/textEditorConstants.js
+++ src/components/TextEditor/textEditorConstants.js
@@ -1,8 +1,34 @@
+const handleKeyDown = e => {
+  e.stopPropagation();
+};
+
 export const modules = {
   clipboard: {
     matchVisual: false,
   },
-  toolbar: '#toolbar',
+  toolbar: {
+    container: '#toolbar',
+    handlers: {
+      link: function customLink(value) {
+        const input = document.querySelector('.ql-tooltip [data-link]');
+        if (value) {
+          const range = this.quill.getSelection();
+          if (range === null || range.length === 0) {
+            return;
+          }
+          const { tooltip } = this.quill.theme;
+          const toolbar = document.getElementById('toolbar');
+          toolbar.appendChild(tooltip.root);
+          tooltip.edit('link', this.quill.getText(range));
+          input.setAttribute('placeholder', 'Enter a link');
+          input.addEventListener('keydown', handleKeyDown);
+        } else {
+          input.removeEventListener('keydown', handleKeyDown);
+          this.quill.format('link', false);
+        }
+      },
+    },
+  },
 };
 
 export const formats = [
