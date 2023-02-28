55644e9aead43ca2655906e0beab984a756b4d28
ozsirma <alican@jotform.com>
2023-02-28 10:56:40 +0300

    feat(static-scene): zoom-pan-pinch feature is supported now by dnd-builder
    Co-Authored-By: Aybüke Ceylan <aybukeceylan@users.noreply.github.com>

--- src/components/Preview/StaticScene.js
+++ src/components/Preview/StaticScene.js
@@ -1,5 +1,9 @@
-import { useEffect, useRef } from 'react';
+import {
+  useCallback, useEffect, useMemo, useRef,
+} from 'react';
 import PropTypes from 'prop-types';
+import cNames from 'classnames';
+import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch';
 import * as classNames from '../../constants/classNames';
 import StaticPage from './StaticPage';
 import { usePropContext } from '../../utils/propContext';
@@ -20,19 +24,11 @@ const StaticScene = ({
   pages,
   presentationPage,
 }) => {
-  const { isFullscreen, showControlsInFullScreen } = usePresentationContext();
+  const { fittedZoom, isFullscreen, showControlsInFullScreen } = usePresentationContext();
   const { acceptedItems, settings } = usePropContext();
-  let { zoom } = useBuilderContext();
+  const { setZoom, zoom } = useBuilderContext();
   const viewPortRef = useRef({});
-  const pageStyles = useRef({});
-  const pageContainerStyles = useRef({});
-  const zoomAdjusterStyles = useRef({});
-  zoom = mode === 'print' ? 1 : zoom;
-  useEffect(() => {
-    if (viewPortRef.current) {
-      viewPortRef.current.scrollTop = lastScrollPosition;
-    }
-  }, []); // set last scroll position after changing mode
+  const transformRefs = useRef([]);
 
   const {
     reportBackgroundColor,
@@ -43,31 +39,45 @@ const StaticScene = ({
   } = settings;
   const width = parseInt(reportLayoutWidth, 10);
   const height = parseInt(reportLayoutHeight, 10);
-  pageStyles.current = {
-    alignItems: 'center',
-    justifyContent: 'center',
-  };
-  pageContainerStyles.current = {
+
+  useEffect(() => {
+    if (mode === 'print') {
+      setZoom(1);
+    }
+  }, [mode]);
+
+  useEffect(() => {
+    if (viewPortRef.current) {
+      viewPortRef.current.scrollTop = lastScrollPosition;
+    }
+  }, []); // set last scroll position after changing mode
+
+  useEffect(() => {
+    transformRefs.current = transformRefs.current.slice(0, pages.length);
+  }, [pages]);
+
+  const pageContainerStyles = useMemo(() => ({
     height,
-    transform: `scale(${zoom})`,
-    transformOrigin: '0 0',
     width,
-  };
-  zoomAdjusterStyles.current = {
-    height: parseFloat((height * zoom).toFixed(1)),
-    margin: '0',
-    width: parseFloat((width * zoom).toFixed(1)),
-  };
-
-  if (viewPortRef.current.clientWidth < zoomAdjusterStyles.current.width) {
-    delete pageStyles.current.justifyContent;
-  }
-
-  if (viewPortRef.current.clientHeight < zoomAdjusterStyles.current.height) {
-    delete pageStyles.current.alignItems;
-  }
+  }), [width, height]);
 
   const transitionStyle = usePageTransition(reportPageTransition, presentationPage);
+
+  const handleZoom = useCallback(zoomPanPinch => {
+    const fixedValue = parseFloat(zoomPanPinch.state.scale.toFixed(2));
+    const roundValue = parseFloat((Math.round((fixedValue * 100)) / 100).toFixed(1));
+    if (roundValue <= 2 && roundValue >= 0.5) {
+      setZoom(roundValue);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (transformRefs.current.length > 0) {
+      for (let i = 0; i < pages.length; i++) {
+        transformRefs.current[i].centerView(zoom);
+      }
+    }
+  }, [pages.length, zoom, isFullscreen]);
 
   return (
     <main
@@ -85,34 +95,50 @@ const StaticScene = ({
           {pages.map((page, index) => {
             const { backgroundColor } = page;
             const style = {
-              ...pageContainerStyles.current,
+              ...pageContainerStyles,
               backgroundColor: backgroundColor ? backgroundColor : reportBackgroundColor || '#fff',
             };
             return (
               <div
                 key={`page_${index.toString()}`}
-                className={`${
-                  classNames.page
-                } ${
-                  slugify(reportLayout)
-                } ${
-                  index === presentationPage ? 'activePage' : ''
-                }`}
-                style={pageStyles.current}
+                className={cNames({
+                  [classNames.page]: true,
+                  [slugify(reportLayout)]: true,
+                  activePage: index === presentationPage,
+                })}
               >
-                <div
-                  className={`jfReport-zoomAdjuster ${mode !== 'preview' ? 'd-flex' : ''}`}
-                  style={zoomAdjusterStyles.current}
+                <TransformWrapper
+                  ref={element => { transformRefs.current[index] = element; }}
+                  centerOnInit={true}
+                  centerZoomedOut={true}
+                  disablePadding={true}
+                  doubleClick={{
+                    step: 0.2,
+                  }}
+                  initialScale={zoom}
+                  maxScale={2}
+                  minScale={0.5}
+                  onZoom={handleZoom}
+                  panning={{
+                    disabled: zoom <= fittedZoom,
+                  }}
+                  pinch={{
+                    step: 1,
+                  }}
                 >
-                  <StaticPage
-                    acceptedItems={acceptedItems}
-                    additionalPageItems={additionalPageItems}
-                    hashCode={hashCode}
-                    itemAccessor={itemAccessor}
-                    items={page.items}
-                    style={style}
-                  />
-                </div>
+                  <TransformComponent
+                    wrapperStyle={{ height: '100%', width: '100%' }}
+                  >
+                    <StaticPage
+                      acceptedItems={acceptedItems}
+                      additionalPageItems={additionalPageItems}
+                      hashCode={hashCode}
+                      itemAccessor={itemAccessor}
+                      items={page.items}
+                      style={style}
+                    />
+                  </TransformComponent>
+                </TransformWrapper>
               </div>
             );
           })}
